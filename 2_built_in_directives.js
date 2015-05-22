b0b0e41d56affc78776dc611f0ce16113785cd3e
nguyennam9696 <nguyennam9696@gmail.com>
2015-05-22 16:21:31 -0700

    learn about directives, modules, controllers, and expressions to make gems

--- 2_built_in_directives.js
+++ 2_built_in_directives.js
@@ -1,23 +1,44 @@
-var gem = {
+var gems = [
+{
     name: 'Dodecahedron',
     price: 2.95,
     description: '. . .',
     canPurchase: true, //only show if it is true
     soldOut: true //hide product if it is sold out
+},
+{
+    name: 'Pentagonal Gem',
+    price: 5.95,
+    description: ". . .",
+    canPurchase: false,
 }
+];
 
 <html ng-app="store">
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
-<body>
-    <div ng-hide="store.product.soldOut" ng-controller="StoreController as store">
-        <h1> {{store.product.name}} </h1>
-        <h2> {{store.product.price}} </h2>
-        <p> {{store.product.description}} </p>
+<body ng-controller="StoreController as store">
+    <div ng-hide="store.product.soldOut" >
+        <h1> {{store.products[0].name}} </h1>
+        <h2> {{store.products[0].price}} </h2>
+        <p> {{store.products[0].description}} </p>
         //only show if canPurchase is true
-        <button ng-show="store.product.canPurchase"> Add to Cart </button>
+        <button ng-show="store.products[0].canPurchase"> Add to Cart </button>
     </div>
 </body>
 </html>
+
+//iterate through array instead of hard coding the index
+//use ng-repeat directive
+//product is the array we want to iterate over
+<body ng-controller="StoreController as store">
+    <div ng-repeat="product in store.products">
+        <h1> {{product.name}} </h1>
+        <h2> {{product.price}} </h2>
+        <p> {{product.description}} </p>
+        //only show if canPurchase is true
+        <button ng-show="product.canPurchase"> Add to Cart </button>
+    </div>
+</body>
